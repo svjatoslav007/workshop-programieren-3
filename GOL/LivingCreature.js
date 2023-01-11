1c79b642d63f44426b4726025224a856cf8b2b93
svjatoslav007 <absenteetitan94@tumo.world>
2023-01-11 16:25:43 +0100

    s3.2

--- GOL/LivingCreature.js
+++ GOL/LivingCreature.js
@@ -3,6 +3,16 @@ class LivingCreature {
         this.x = x;
         this.y = y;
         this.multiply=0;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x    , this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y    ],
+            [this.x + 1, this.y    ],
+            [this.x - 1, this.y + 1],
+            [this.x    , this.y + 1],
+            [this.x + 1, this.y + 1]
+        ]
        
     }
 }
--- GOL/Toadstool.js
+++ GOL/Toadstool.js
@@ -2,18 +2,8 @@
 
 class Toadstool extends LivingCreature {
     constructor(x, y) {
-        this.x = x
-        this.y = y
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x    , this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y    ],
-            [this.x + 1, this.y    ],
-            [this.x - 1, this.y + 1],
-            [this.x    , this.y + 1],
-            [this.x + 1, this.y + 1]
-        ]
+        super()
+       
     }
     eat() {
         let cells = this.directions
