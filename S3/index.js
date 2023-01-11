07bcbad268e9a8d899a90e76325fdd0ca139cb9d
svjatoslav007 <absenteetitan94@tumo.world>
2023-01-11 17:46:12 +0100

    meine erste server

--- S3/index.js
+++ S3/index.js
@@ -1,6 +1,17 @@
-const os =require("os");
-let message = "mein Betribsystem ist ";
-function main (){
-    console.log(message + os.plattform)
-}
+const express = require("express");
+const app =express();
+
+app.get("/", function(req, res){
+    res.send("wallah kriese")
+});
+
+
+app.listen(3000,function(){
+    console.log("server leuft auf port 3000")
+});
+
+app.get("/name/:name", function(req, res){
+    let name = req.params.name;
+    res.send("<h1>Hello " + name +"</h1>");
+ });
 
