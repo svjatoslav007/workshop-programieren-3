c8978c974a001c86ec19389d76b66d0f7f8505c3
svjatoslav007 <absenteetitan94@tumo.world>
2023-01-11 16:39:15 +0100

    s3.6

--- GOL/LivingCreature.js
+++ GOL/LivingCreature.js
@@ -4,16 +4,21 @@ class LivingCreature {
         this.y = y;
         this.multiply=0;
         this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x    , this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y    ],
-            [this.x + 1, this.y    ],
-            [this.x - 1, this.y + 1],
-            [this.x    , this.y + 1],
-            [this.x + 1, this.y + 1]
-        ]
-       
+            [this.x -1, this.y -1],
+            [this.x, this.y-1],
+            [this.x+1, this.y-1],
+            [this.x-1, this.y],
+            [this.x+1, this.y],
+            [this.x-1, this.y+1],
+            [this.x, this.y+1],
+            [this.x+1, this.y+1]
+        ];
+
+        
+    
+           
+        
+         
     }
     chooseFields(character){
         this.newDirections();
