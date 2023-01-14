e19a5910aeaab9ed6e4a136abde2b583f7a1de7b
svjatoslav007 <absenteetitan94@tumo.world>
2023-01-14 11:08:43 +0100

    s4a4

--- S3/index.js
+++ S3/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app =express();
 
+app.use(express.static("../GOL"));
+
+ app.get("/", function(req, res){
+     res.redirect("/index.html");
+ })
+
 app.get("/", function(req, res){
     res.send("wallah kriese")
 });
@@ -15,8 +21,16 @@ app.get("/name/:name", function(req, res){
     res.send("<h1>Hello " + name +"</h1>");
  });
 
- app.get("/url/:url/search:/search", function(req, res){
-    let url = res.redirect("http://google.com") 
-    let search =req.params.serach;
-    res.send("<h1>Hello " + url+search+"</h1>");
+ app.get("/google/:search", function(req, res){
+    let search =req.params.search;
+    res.redirect("http://google.com/search?q="+search) ;
  });
+ app.get("/*", function(req, res){
+    res.status(404).send("<h1>Error 404</h1>") ;
+ });
+
+
+ 
+ app.listen(3000,function(){
+     console.log("GOL")
+ })
