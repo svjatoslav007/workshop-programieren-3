f1a2edc7028ecb861907d2c6a74b9704c318e27f
svjatoslav007 <absenteetitan94@tumo.world>
2023-01-14 10:28:31 +0100

    s4a2

--- S3/index.js
+++ S3/index.js
@@ -15,7 +15,8 @@ app.get("/name/:name", function(req, res){
     res.send("<h1>Hello " + name +"</h1>");
  });
 
- app.get("/url/:url", function(req, res){
+ app.get("/url/:url/search:/search", function(req, res){
     let url = res.redirect("http://google.com") 
-    res.send("<h1>Hello " + url+"</h1>");
+    let search =req.params.serach;
+    res.send("<h1>Hello " + url+search+"</h1>");
  });
