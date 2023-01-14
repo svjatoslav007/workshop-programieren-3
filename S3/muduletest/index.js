2b48ebd408331211bb353b0ffe45218405b9039d
svjatoslav007 <absenteetitan94@tumo.world>
2023-01-14 11:42:52 +0100

    module

--- S3/muduletest/index.js
+++
@@ -1,5 +0,0 @@
-const Square =require("./square.js");
-
-let mySquare =new Square(5);
-console.log(mySquare.side);
-console.log("fläche ist :",mySquare.getArea());
---
+++ muduletest/index.js
@@ -0,0 +1,10 @@
+//const Square =require("./square.js");
+
+const square = require("./square");
+
+//let mySquare =new Square(5);
+//console.log(mySquare.side);
+square.myTest();
+let squareobj =new square.Square(5);
+console.log(squareobj.side);
+Square.myTest();
